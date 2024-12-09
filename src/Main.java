1a76957e323cc1f81f0550cee030bdcf712962ea
Bkarybaev <baielkarybaev@gmail.com>
2024-12-09 20:13:10 +0600

    ArrayList

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,28 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        ArrayList<Integer> i = new ArrayList<>();
+        for (int j = 1; j <= 50; j++) {
+            i.add(new Random().nextInt(1, 100));
         }
+
+        ArrayList<Integer> i2 = new ArrayList<>();
+        ArrayList<Integer> i3 = new ArrayList<>();
+        for (int i1 = 0; i1 < i.size(); i1++) {
+            if (i.get(i1) % 2 != 0) {
+                i2.add(i.get(i1));
+            }else if (i.get(i1) % 2 == 0) {
+                i3.add(i.get(i1));
+            }
+        }
+
+
+        System.out.println(i);
+        System.out.println("tak san");
+        System.out.println(i2);
+        System.out.println("jup san");
+        System.out.println(i3);
     }
 }
